f78db0c713460463e38f2e5244295e67f9ae776a
DeltrivX <238145918+deltrivx@users.noreply.github.com>
2026-05-18 19:06:20 +0800

    fix: change Kris to DeltrivX in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <p>© {currentYear} Kris. Made with ❤️ and lots of ☕</p>
+            <p>© {currentYear} DeltrivX. Made with ❤️ and lots of ☕</p>
         </footer>
     );
 }
