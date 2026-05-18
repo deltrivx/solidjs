4896b29c9dd3f16cf587e71150ef574512c56eba
DeltrivX <238145918+deltrivx@users.noreply.github.com>
2026-05-18 22:27:33 +0800

    fix: clean index.jsx - remove deleted routes

--- src/index.jsx
+++ src/index.jsx
@@ -16,31 +16,18 @@ render(
             <Title>DeltrivX | 个人主页</Title>
             <Meta name="description" content="DeltrivX - 全栈开发者、AI探索者、开源贡献者" />
             <Home />
-          </>
-        )} />
         <Route path="/about" component={() => (
           <>
             <Title>DeltrivX | 关于我</Title>
             <Meta name="description" content="了解 DeltrivX 的背景和经历" />
             <About />
-          </>
-        )} />
-            <Skills />
-          </>
-        )} />
         <Route path="/projects" component={() => (
           <>
             <Title>DeltrivX | 精选项目</Title>
             <Meta name="description" content="DeltrivX 引以为豪的作品" />
             <Projects />
-          </>
-        )} />
-            <Contact />
-          </>
-        )} />
 </>
-        )} />
-      </HashRouter>
+              </HashRouter>
     </MetaProvider>
   ),
   document.getElementById('root')
